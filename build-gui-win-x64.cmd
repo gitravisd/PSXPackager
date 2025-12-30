6ca0c1cae44d67e6bec8a63abb98e4e6ca23cb03
gitravisd <travisd@live.com.au>
2025-12-30 17:18:14 +1100

    Copy working now, build-gui-win-x64.cmd
    cp is not a windows command

--- build-gui-win-x64.cmd
+++ build-gui-win-x64.cmd
@@ -1,3 +1,3 @@
 SET BUILD_PATH=.\build\PsxPackagerGUI
 dotnet publish .\PSXPackagerGUI\PSXPackagerGUI.csproj -c Release -r win-x64 -o %BUILD_PATH% --no-self-contained /p:PublishSingleFile=true /p:PublishReadyToRun=false /p:DebugType=None /p:DebugSymbols=false
-cp .\Popstation.Database\Resources\gameInfo.db %BUILD_PATH%\Resources\gameInfo.db
+copy .\Popstation.Database\Resources\gameInfo.db %BUILD_PATH%\Resources\gameInfo.db
